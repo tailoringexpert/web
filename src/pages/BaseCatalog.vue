162c24fdc6b30c406e6ea5bd3d308ec61766df9e
Michael Bädorf <tailoringexpert@michael-baedorf.de>
2023-04-03 15:30:27 +0200

    moved components -> pages
    Signed-off-by: Michael Bädorf <tailoringexpert@michael-baedorf.de>

---
+++ src/pages/BaseCatalog.vue
@@ -0,0 +1,9 @@
+<template src='@/pages/BaseCatalog/index.template.html'></template>
+<script>
+import { data } from "@/pages/BaseCatalog/index.js";
+
+export default {
+  mixins: [data],
+}
+
+</script>
