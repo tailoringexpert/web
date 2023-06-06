e8bc5af887c76424588fe1593a96236449894bcc
Michael Bädorf <tailoringexpert@michael-baedorf.de>
2023-06-06 12:34:51 +0200

    feature/remove_components

--- src/components/Project.vue
+++
@@ -1,8 +0,0 @@
-<template src='@/components/Project/index.template.html'></template>
-<script>
-import { data } from "@/components/Project/index.js";
-
-export default {
-  mixins: [data],
-}
-</script>
--- src/components/TailoringNew.vue
+++
@@ -1,8 +0,0 @@
-<template src='./TailoringNew/index.template.html'></template>
-<script>
-import { data } from "./TailoringNew/index.js";
-
-export default {
-  mixins: [data],
-}
-</script>
