f2310c70ec444f0b35c747c7ad0d90050ac2d408
Michael Bädorf <tailoringexpert@m.familiebaedorf.de>
2026-03-28 13:40:37 +0000

    adaptions for devcontainer

--- vite.config.mjs
+++ vite.config.mjs
@@ -12,7 +12,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    // MIME-Types Mapper für die gängigsten statischen Dateien
+    const localProxies = JSON.parse(process.env.VITE_PROXY_DIRS || '{}')
     const mimeTypes = {
         '.html': 'text/html',
         '.js': 'text/javascript',
@@ -33,32 +33,20 @@ export default ({ mode }) => {
 
     return defineConfig({
         define: {
-            AUTH_REQUIRED: JSON.parse(`"${process.env.VITE_AUTH_REQUIRED}"`.toLowerCase()),
-            APP_TENANT: `"${process.env.VITE_APP_TENANT}"`,
             WEB_NAME: JSON.stringify(packageJson.name),
             WEB_VERSION: JSON.stringify(packageJson.version),
             IDM_URL: `"${process.env.VITE_IDM_URL}"`,
             IDM_REALM: `"${process.env.VITE_IDM_REALM}"`,
-            IDM_CLIENT: `"${process.env.VITE_IDM_CLIENT}"`,
+            IDM_CLIENT: `"${process.env.VITE_IDM_CLIENT}"`
         },
         server: {
             port: 3000,
             headers: {
-                'Access-Control-Allow-Origin': '*',
-                //'X-TENANT': process.env.VITE_APP_TENANT
+                'Access-Control-Allow-Origin': '*'
             },
             proxy: {
                 '^/api': {
                     target: process.env.VITE_API_TARGET
-                },
-                '^/assets': {
-                    target: process.env.VITE_ASSETS_TARGET
-                },
-                'impressum.html': {
-                    target: process.env.VITE_ASSETS_TARGET
-                },
-                'dataprotection.html': {
-                    target: process.env.VITE_ASSETS_TARGET
                 }
             }
         },
@@ -70,59 +58,28 @@ export default ({ mode }) => {
             Components({
                 resolvers: [PrimeVueResolver()]
             }),
-
-           {
+            {
                 name: 'native-static-file-server',
                 configureServer(server) {
-                    console.log("conf");
-                    server.middlewares.use('/static/arsu', (req, res, next) => {
-                        console.log("sever");
-                       const baseDir = '/home/developer/git/www';
-                        // Pfad bereinigen (Query Params entfernen)
-                        const urlPath = req.url; //.split('?')[0];
-                        const filePath = path.join(baseDir, urlPath);
+                    Object.entries(localProxies).forEach(([urlPath, systemPath]) => {
+                        server.middlewares.use(urlPath, (req, res, next) => {
+                            const baseDir = path.resolve(process.cwd(), systemPath);
+                            const relativeFilePath = req.url.split('?')[0];
+                            const filePath = path.join(baseDir, relativeFilePath);
 
-                        console.log("hall")
-                        // Sicherheit: Prüfen, ob der Pfad noch innerhalb von baseDir liegt
-                        if (!filePath.startsWith(baseDir)) {
-                            return next();
-                        }
-
-                        console.log("!shd")
-                        if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
-                            const ext = path.extname(filePath).toLowerCase();
-                            const contentType = mimeTypes[ext] || 'application/octet-stream';
-
-                            console.log(contentType);
-                            res.setHeader('Content-Type', contentType);
-                            res.setHeader('Cache-Control', 'no-cache');
-
-                            // Stream statt readFileSync (Performance!)
-                            const stream = fs.createReadStream(filePath);
-                            stream.pipe(res);
-                        } else {
-                            next();
-                        }
+                            if (fs.existsSync(filePath) && fs.lstatSync(filePath).isFile()) {
+                                const ext = path.extname(filePath).toLowerCase();
+                                const contentType = mimeTypes[ext] || 'application/octet-stream';
+                                res.setHeader('Content-Type', contentType);
+                                res.setHeader('Cache-Control', 'no-cache');
+                                fs.createReadStream(filePath).pipe(res);
+                            } else {
+                                next();
+                            }
+                        });
                     });
                 }
             }
-
-            // 2. Sirv als Custom Plugin integrieren
-            // {
-            //     name: '/static/arsu',
-            //     configureServer(server) {
-            //         // Ersetze 'C:/dein/pfad' durch dein tatsächliches Verzeichnis
-            //         console.log("sirv");
-            //         const localPath = fileURLToPath(new URL('/home/developer/git/www', import.meta.url));
-            //         //const localPath = path.resolve(__dirname, '/home/developer/git/www');
-
-            //         // Serviert den Ordner unter http://localhost:3000/local-files
-            //         server.middlewares.use(
-            //             '/static/arsu',
-            //             sirv(localPath, { dev: true, dotfiles: false })
-            //         );
-            //     }
-            // }
         ],
         build: {
             sourcemap: true,
